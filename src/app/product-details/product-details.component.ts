f47d656fec80a1a08bab033f4e736c9db4a948cc
RafaelSVMartins <rafaelviniciusdasilvamartins@gmail.com>
2022-04-04 10:04:23 -0300

    Second part of the cart is already fineshed.

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Product, products } from '../products';
+import { CartService } from '../cart.service';
 
 @Component({
   selector: 'app-product-details',
@@ -10,8 +11,15 @@ import { Product, products } from '../products';
 export class ProductDetailsComponent implements OnInit {
   product: Product | undefined;
 
-  constructor(private route: ActivatedRoute) {}
+  constructor(
+    private route: ActivatedRoute,
+    private cartService: CartService
+  ) {}
 
+  addToCart(product: Product) {
+    this.cartService.addToCart(product);
+    window.alert('Your cart has been added to the car!');
+  }
   ngOnInit(): void {
     const routeParams = this.route.snapshot.paramMap;
     const productIdFormRoute = Number(routeParams.get('productId'));
