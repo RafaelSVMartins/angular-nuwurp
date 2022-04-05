3313e01621e0d9839ba78edce4b32cec856262e2
RafaelSVMartins <rafaelviniciusdasilvamartins@gmail.com>
2022-04-05 09:19:21 -0300

    Angular with forms is already finished

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,29 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
-import {CartService} from '../cart.service';
+import { CartService } from '../cart.service';
 
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
-  styleUrls: ['./cart.component.css']
+  styleUrls: ['./cart.component.css'],
 })
 export class CartComponent implements OnInit {
   items = this.cartService.getItems();
-  checkout = this.formBuilder.group({
-    name:'',
-    address:''
-  })
+  checkoutForm = this.formBuilder.group({
+    name: '',
+    address: '',
+  });
 
   constructor(
     private cartService: CartService,
-    private formBuilder: FormBuilder) { }
-   
-    onSubmit():void {
-      this.items = this.cartService.clearCart();
-      console.warn('Your order has been submitted', this.checkout.value);
-      this.checkout.reset();
-    }
-  ngOnInit(): void {
-  }
+    private formBuilder: FormBuilder
+  ) {}
 
+  onSubmit(): void {
+    this.items = this.cartService.clearCart();
+    console.warn('Your order has been submitted', this.checkoutForm.value);
+    this.checkoutForm.reset();
+  }
+  ngOnInit(): void {}
 }
